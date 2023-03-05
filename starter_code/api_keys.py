3d41862d07121e49ee76992fdd2c3825e97bc058
Arkmandina <amandrose@gmail.com>
2023-03-05 12:33:14 -0500

    setup for folders

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = d1791700ce52e358213a467b4da6927e
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = 8218bd41267844a28d5914c52f7259d7
